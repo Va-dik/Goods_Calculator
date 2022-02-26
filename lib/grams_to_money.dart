6a4c72b2b6641cfe9c4e082ac3164edf7d46cd20
Va-dik <bin_448@mail.ru>
2022-02-27 02:03:32 +0300

    Updating

--- lib/grams_to_money.dart
+++ lib/grams_to_money.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:goods_calculator/localization/locale_ru.dart';
 
@@ -9,7 +11,10 @@ class GramsToMoney extends StatefulWidget {
 }
 
 class _GramsToMoneyState extends State<GramsToMoney> {
-  TextEditingController _controller = TextEditingController();
+  List<TextEditingController> _controller = [
+    for (int i = 0; i < 3; i++) TextEditingController()
+  ];
+
   double money = 0;
 
   @override
@@ -25,34 +30,58 @@ class _GramsToMoneyState extends State<GramsToMoney> {
           color: Colors.blue[200],
         ),
         child: Stack(children: [
-          money == 0 ? Text(mapa.keys.first) : Text(mapa.values.first),
+          money == 0
+              ? Text(
+                  mapa.keys.first,
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                )
+              : Text(mapa.values.first),
           Align(
-            alignment: Alignment.topCenter,
+            alignment: const Alignment(-1, 1),
             child: SizedBox(
               height: 50,
               width: 50,
               child: TextField(
-                controller: _controller,
+                controller: _controller[0],
               ),
             ),
           ),
           Align(
-            alignment: Alignment.topLeft,
+            alignment: const Alignment(-0.4, -1),
             child: SizedBox(
               height: 50,
-              width: 50,
+              width: 60,
               child: TextField(
-                controller: _controller,
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                      vertical: 10), //Change this value to custom as you like
+                  isDense: true,
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(color: Colors.red, width: 2),
+                  ),
+                  // and add this line
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(color: Colors.cyan, width: 2),
+                  ),
+                ),
+                controller: _controller[1],
               ),
             ),
           ),
           Align(
-            alignment: Alignment.topRight,
+            alignment: const Alignment(1, 1),
             child: SizedBox(
               height: 50,
               width: 50,
               child: TextField(
-                controller: _controller,
+                controller: _controller[2],
               ),
             ),
           ),
--- lib/localization/locale_ru.dart
+++ lib/localization/locale_ru.dart
@@ -1,5 +1,9 @@
 Map<String, String> mapa = {
-  "Введите цену товара за киллограм": "Product price per kilogram",
-  "Сколько взяли грамм": "How many grams taken"
+  "Введите цену товара за килограмм" : "Product price per kilogram",
+  "Сколько взяли грамм" : "How many grams taken",
+  "RU" : "грамм",
+  "EN" : "gram",
+  
 };
 
+List<String> locale = ["грамм, gram"];
